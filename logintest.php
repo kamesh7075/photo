7610edb9fc8ef62c21e531ce5b638008b36252ae
Kameshwaran S <kamesh7075@gmail.com>
2024-12-29 04:06:37 +0000

    Initial commit

--- logintest.php
+++ logintest.php
@@ -5,15 +5,34 @@ include 'libs/load.php';
 
 $user = "testdata";
 $pass = "tested";
-
-$resultlog = user::login($user, $pass);
-// print_r($resultlog);
-if($resultlog)
+$resultlog = null;
+if(isset($_GET['logout']))
 {
-  echo "login success, $resultlog[username]\n";
+  session_destroy();
+  die("session destroyed, <a href='logintest.php'>Login Again</a>");
+}
+if(session::get('is_loggedin'))
+{
+  $userdata = session::get('session_user'); 
+  printf("welcome back, $userdata[username]");
+  $resultlog = $userdata;
 }
 else
 {
-  echo "login failed \n";
+  printf("no session found, trying to login now.");
+  $resultlog = user::login($user, $pass);
+  if($resultlog)
+  {
+    echo "login success, $resultlog[username]\n";
+    session::set('is_loggedin', true);
+    session::set('session_user', $resultlog);
+  }
+  else
+  {
+    echo "login failed \n";
+  }
 }
+echo <<<EOL
+<br><br><a href = "logintest.php?logout">Logout</a>
+EOL;
 ?>
